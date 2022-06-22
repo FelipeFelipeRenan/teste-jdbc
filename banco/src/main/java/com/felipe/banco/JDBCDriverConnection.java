783f8ad7686f8ef13f4fac0a3d28ed5ad05ff4e7
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-06-22 15:57:28 -0300

    First commit

---
+++ banco/src/main/java/com/felipe/banco/JDBCDriverConnection.java
@@ -0,0 +1,84 @@
+package com.felipe.banco;
+
+/**
+ * Hello world!
+ *
+ */
+
+import java.util.Scanner;
+//import com.felipe.banco.UserDAO;
+
+public class JDBCDriverConnection {
+    UserDAO user = new UserDAO();
+
+    public static void main(String[] args) {
+        // 1Statement statement = connection.createStatement();
+
+        Scanner sc = new Scanner(System.in);
+        String opcao, nome = null;
+        int opt = 0, id = 0;
+        
+        do {
+            System.out.println("------------------------------------------------");
+            System.out.println("-----BDSM - Banco de Dados Simples Multiuso-----");
+            System.out.println("------------------------------------------------");
+            System.out.println("Digite 1 para inicializar o banco");
+            System.out.println("Digite 2 para adicionar um novo registro");
+            System.out.println("Digite 3 para visualizar registros existentes");
+            System.out.println("Digite 4 para remover o registro indicado ");
+            System.out.println("Digite 5 para atualizar o registro indicado ");
+            opt = sc.nextInt();
+            switch (opt) {
+                case 1:
+                    UserDAO.connect(1, id, nome);
+                    break;
+                case 2:
+                    System.out.println("Adicionando na base de dados");
+                    System.out.println("Entre com o ID: ");
+                    id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Entre com o nome: ");
+                    nome = sc.nextLine();
+                    UserDAO.connect(2, id, nome);
+                  
+
+                    break;
+                case 3:
+                    UserDAO.connect(3, id, nome);
+                    break;
+                case 4:                   
+                    System.out.println("Excluindo da base de dados");
+                    System.out.println("Entre com o ID: ");
+                    id = sc.nextInt();
+                    UserDAO.connect(4,id,nome);
+                    sc.nextLine();
+                    
+                    break;
+                
+                case 5:
+                    System.out.println("Modificando o registro");
+                    System.out.println("Entre com o ID: ");
+                    id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Entre com o nome");
+                    nome = sc.nextLine();
+                    UserDAO.connect(5,id,nome);
+                    sc.nextLine();
+
+
+
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+            sc.nextLine();
+            System.out.println("Deseja continuar? Y | N");
+            opcao = String.valueOf(sc.nextLine());
+        } while (opcao.equals("Y") || opcao.equals("y"));
+
+        sc.close();
+    }
+}
---
+++ banco/src/main/java/com/felipe/banco/UserDAO.java
@@ -0,0 +1,60 @@
+package com.felipe.banco;
+
+import java.sql.*;
+
+public class UserDAO {
+
+    public static void connect(int opt, int idUser, String name) {
+        String dbInput;
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:banco.db")) {
+
+            System.out.println("Conexão realizada !!!!");
+
+            Statement statement = connection.createStatement();
+
+            // criando uma tabela
+            switch (opt) {
+                case 1:
+                    statement.execute("CREATE TABLE IF NOT EXISTS RC_TEST( ID INTEGER, NOME VARCHAR )");
+
+                    break;
+
+                case 2:
+                    dbInput = String.format("INSERT INTO RC_TEST( ID, NOME) VALUES (%d, '%s')", idUser, name);
+                    // inserindo registros
+                    statement.execute(dbInput);
+
+                    break;
+
+                case 3:
+                    // lendo os registros
+                    PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RC_TEST");
+                    ResultSet resultSet = stmt.executeQuery();
+
+                    while (resultSet.next()) {
+                        Integer id = resultSet.getInt("ID");
+                        String nome = resultSet.getString("NOME");
+
+                        System.out.println(id + " - " + nome);
+                    }
+                    break;
+                case 4:
+                    dbInput = String.format("DELETE FROM RC_TEST WHERE ID=%d", idUser);
+                    // inserindo registros
+                    statement.execute(dbInput);
+
+                    break;
+                case 5:
+                    dbInput = String.format("UPDATE RC_TEST SET NOME=%s WHERE ID=%d", name,idUser);
+                    break;
+                default:
+                    System.out.println("Opção indisponivel!");
+                    break;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
